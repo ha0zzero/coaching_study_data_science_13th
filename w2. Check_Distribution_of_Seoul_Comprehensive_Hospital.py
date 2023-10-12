b0f9ef77589eebbaa80e8131554eb5404f5b711e
ha0zzero <ha0zzero@gmail.com>
2023-10-13 01:16:56 +0900

    w2. Check_Distribution_of_Seoul_Comprehensive_Hospital 완료

--- w2. Check_Distribution_of_Seoul_Comprehensive_Hospital.py
+++ w2. Check_Distribution_of_Seoul_Comprehensive_Hospital.py
@@ -13,6 +13,8 @@
 #     name: python3
 # ---
 
+# %config Completer.use_jedi = False
+
 # + [markdown] id="0mlwJLwy6Lo5"
 # [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/corazzon/boostcourse-ds-510/blob/master/open-data-analysis-input.ipynb)
 #
@@ -98,6 +100,7 @@ df.info()
 
 # + id="0D8j35-A6Lpl"
 # 컬럼명만 출력해 봅니다.
+df.columns
 
 
 # + [markdown] id="h-zPZT6a6Lpm"
@@ -105,21 +108,32 @@ df.info()
 
 # + id="UlnY-0jb6Lpm"
 # 데이터 타입만 출력합니다.
+df.dtypes
 
 
 # + [markdown] id="hqhUsmpV6Lpn"
 # ## 결측치
+# -
+
+#python
+True == 1
 
 # + id="nafsoihK6Lpo"
+null_count = df.isnull().sum()
+null_count
 
 
 # + id="Elh4UC6f6Lpo"
 # 위에서 구한 결측치를 .plot.bar 를 통해 막대그래프로 표현합니다.
+null_count.plot.barh(figsize=(5, 7))
 
 
 # + id="eK6JzoO06Lpp"
 # 위에서 계산한 결측치 수를 reset_index 를 통해 데이터프레임으로 만들어 줍니다.
 # df_null_count 변수에 결과를 담아서 head 로 미리보기를 합니다.
+
+df_null_count = null_count.reset_index()
+df_null_count.head(1)
 
 
 # + [markdown] id="Ci1F_GiE6Lpq"
@@ -127,6 +141,8 @@ df.info()
 
 # + id="os2eLKZl6Lpr"
 # df_null_count 변수에 담겨있는 컬럼의 이름을 "컬럼명", "결측치수"로 변경해 줍니다.
+df_null_count.columns = ["컬럼명", "결측치수"]
+df_null_count.head(1)
 
 
 # + [markdown] id="KCjhPs0b6Lpr"
@@ -135,6 +151,8 @@ df.info()
 # + id="SobxGLlh6Lps"
 # df_null_count 데이터프레임에 있는 결측치수 컬럼을 sort_values 를 통해 정렬해서
 # 결측치가 많은 순으로 상위 10개만 출력합니다.
+df_null_count_top = df_null_count.sort_values(by="결측치수", ascending=False).head(10)
+df_null_count_top
 
 
 # + [markdown] id="8eCKXo2N6Lps"
@@ -142,23 +160,32 @@ df.info()
 
 # + id="FGKqTLh_6Lpt"
 # 지점명 컬럼을 불러옵니다.
+# NaN == Not a Number 의 약자. 즉, 결측치.
+df["지점명"].head()
 
 
 # + id="UTHz8dy76Lpt"
 # "컬럼명" 이라는 컬럼의 값만 가져와서 drop_columns 라는 변수에 담습니다.
+drop_columns = df_null_count_top["컬럼명"].tolist()
+drop_columns
 
 
 # + id="qyiDMaoR6Lpu"
 # drop_columns 변수로 해당 컬럼 정보만 데이터프레임에서 가져옵니다.
+df[drop_columns].head()
 
 
 # + [markdown] id="fyX62k-46Lpu"
 # ## 제거하기
 
 # + id="miQcWeHh6Lpv"
+print(df.shape)
+df = df.drop(drop_columns, axis = 1)
+print(df.shape)
 
 
 # + id="sAkWqMpb6Lpv"
+df.info()
 
 
 # + [markdown] id="EVYgbst16Lpw"
@@ -169,22 +196,27 @@ df.info()
 
 # + id="yCHoU4Uq6Lpx"
 # 평균값
+df["위도"].mean()
 
 
 # + id="VK03_HWt6Lpx"
 # 중앙값
+df["위도"].median()
 
 
 # + id="HCf2TcZp6Lpy"
 # 최댓값
+df["위도"].max()
 
 
 # + id="XuEtesR26LqY"
 # 최솟값
+df["위도"].min()
 
 
 # + id="Y9qVLMdq6LqZ"
 # 개수
+df["위도"].count()
 
 
 # + [markdown] id="tokOoXxO6Lqa"
@@ -197,14 +229,18 @@ df.info()
 
 # + id="r5_r5Mbv6Lqb"
 # 위도를 describe로 요약해 봅니다.
+df["위도"].describe()
 
 
 # + id="uifDHvz06Lqd"
 # 2개의 컬럼을 describe 로 요약합니다.
+df[["위도", "경도"]].describe()
 
 
 # + id="wsarkAE86Lqe"
 # describe로 문자열 데이터타입의 요약을 봅니다.
+
+df.describe(include="object")
 
 
 # + [markdown] id="5o6RLIwf6Lqf"
@@ -213,27 +249,34 @@ df.info()
 
 # + id="VEGav34G6Lqg"
 # "상권업종대분류명"
+df["상권업종대분류명"].unique()
 
 
 # + id="2Zwk0Bg76Lqg"
+df["상권업종대분류명"].nunique()
 
 
 # + id="IRskVfNC6Lqh"
 # "상권업종중분류명"
+df["상권업종중분류명"].unique()
 
 
 # + id="IB5w3Of46Lqi"
+df["상권업종중분류명"].nunique()
 
 
 # + id="90JglxJc6Lqj"
 # "상권업종소분류명"
+df["상권업종소분류명"].unique()
 
 
 # + id="Agjf9VG26Lqk"
+df["상권업종소분류명"].nunique()
 
 
 # + id="dFTGgT4F6Lql"
 # nunique 대신 len 을 사용할 수도 있습니다.
+len(df["상권업종소분류명"].unique())
 
 
 # + [markdown] id="Y3E2Av-_6Lqm"
@@ -243,55 +286,77 @@ df.info()
 # + id="kFBk7Riv6Lqm"
 # value_counts 를 사용하면 카테고리 형태의 데이터 갯수를 세어볼 수 있습니다.
 # 시도코드를 세어봅니다.
+df["시도명"].head()
 
 
 # + id="wSxPIcqJ6Lqn"
 # 시도명을 세어봅니다.
+city = df["시도명"].value_counts()
+city
 
 
 # + id="QYFSOI-l6Lqn"
 # normalize=True 옵션을 사용하면 비율을 구할 수 있습니다.
+city_normalize = df["시도명"].value_counts(normalize=True)
+city_normalize
 
 
 # + id="SZIyIipM6Lqo"
 # Pandas 에는 plot기능을 내장하고 있습니다.
 # 위에서 분석한 시도명 수를 막대그래프로 표현해 봅니다.
+city.plot.barh()
+# -
 
+
+# 파이차트는 보기가 불편함.
 
 # + id="9sMZ5M5Q6Lqp"
 # 판다스의 plot.pie()를 사용해서 파이그래프를 그려봅니다.
+city_normalize.plot.pie(figsize=(7, 7))
 
 
 # + id="4pbWg6076Lqq"
+# seaborn 의 countplot 으로 그려봅니다.
+# 씨본을 사용하면 좀 더 예쁘게 시각화할 수 있음
+c = sns.countplot(data=df, y="시도명")
 
 
 # + id="CnVeHCwX6Lqq"
 # "상권업종대분류명"으로 갯수를 세어봅니다.
+df["상권업종대분류명"].value_counts()
 
 
 # + id="03HQSMD26Lqr"
 # "상권업종중분류명"으로 갯수를 세어봅니다.
+c= df["상권업종중분류명"].value_counts()
+c
 
 
 # + id="CI2ygfJO6Lqr"
 # normalize=True 를 사용해 비율을 구해봅니다.
+n = df["상권업종중분류명"].value_counts(normalize=True)
+n
 
 
 # + id="hamEKUro6Lqs"
 # 판다스의 plot.bar()를 사용해서 막대그래프를 그려봅니다.
+c.plot.bar(rot = 0)
 
 
 # + id="xXFk625J6Lqt"
 # 판다스의 plot.pie()를 사용해서 파이그래프를 그려봅니다.
+n.plot.pie()
 
 
 # + id="wS8AcvBE6Lqu"
 # "상권업종소분류명" 에 대한 그룹화 된 값을 카운트 합니다.
+c = df["상권업종소분류명"].value_counts()
 
 
 # + id="Zfy3XZE46Lqv"
 # "상권업종소분류명"으로 갯수를 세어봅니다.
 # 판다스의 plot.bar()를 사용해서 막대그래프를 그려봅니다.
+c.plot.barh(figsize=(7, 8), grid=True)
 
 
 # + [markdown] id="BkhwCAow6Lqw"
@@ -302,6 +367,8 @@ df.info()
 # "상권업종중분류명"이 "약국/한약방"인 데이터만 가져와서
 # df_medical 이라는 변수에 담아봅니다.
 # 그리고 head()를 통해 미리보기를 합니다.
+df_medical = df[df["상권업종중분류명"] == "약국/한약방"].copy()
+df_medical.head(1)
 
 
 # + id="PAjM7d1x6Lqx"
@@ -310,25 +377,32 @@ df.info()
 # 이 기능을 통해 "상권업종중분류명" 만 가져옵니다.
 # 그리고 가져온 결과를 value_counts 를 통해 중분류의 갯수를 세어봅니다.
 # shop_type
+df.loc[df["상권업종대분류명"] == "의료","상권업종중분류명"].value_counts()
 
 
 # + id="2HPk27V_6Lqy"
 # 위와 똑같은 기능을 수행하는 코드입니다. 아래와 같이 한 줄에 표현할 수도 있습니다.
 # df.loc[df["상권업종대분류명"] == "의료", "상권업종중분류명"].value_counts()
+m = df["상권업종대분류명"] == "의료"
+df.loc[m, "상권업종중분류명"].value_counts()
 
 # + id="f9DIlXcJ6Lqz"
 # 유사의료업만 따로 모아봅니다.
+df_medi = df[df["상권업종중분류명"] == "유사의료업"]
+df_medi.shape
 
 
 # + id="I7biIH7b6Lqz"
 # 상호명을 그룹화해서 갯수를 세어봅니다.
 # value_counts 를 사용해서 상위 10개를 출력합니다.
+df["상호명"].value_counts().head(10)
 
 
 # + id="4ENPz-W56Lq0"
 # 유사의료업만 df_medi 변수에 담겨져 있습니다.
 # df_medi 변수에서 상호명으로 갯수를 세어봅니다.
 # 가장 많은 상호 상위 10개를 출력해 봅니다.
+df_medi["상호명"].value_counts().head(10)
 
 
 # + [markdown] id="s_mzuRIi6Lq1"
@@ -338,6 +412,9 @@ df.info()
 # "상권업종소분류명"이 "약국" 인 것과
 # "시도명" 이 "서울특별시"인 데이터만 가져옵니다.
 # df_seoul_drug
+df_seoul_drug = df[(df["상권업종소분류명"] == "약국") & (df["시도명"] == "서울특별시")]
+print(df_seoul_drug.shape)
+df_seoul_drug.head()
 
 # + [markdown] id="BADEGXlp6Lq2"
 # ### 구별로 보기
@@ -345,23 +422,31 @@ df.info()
 # + id="Uy-ZA0RJ6Lq2"
 # 위에서 색인한 데이터로 "시군구명"으로 그룹화 해서 갯수를 세어봅니다.
 # 구별로 약국이 몇개가 있는지 확인해 봅니다.
+c = df_seoul_drug["시군구명"].value_counts()
+c.head()
 
 
 # + id="5Ju2rQg-6Lq3"
 # normalize=True 를 통해 비율을 구해봅니다.
+n = df_seoul_drug["시군구명"].value_counts(normalize=True)
+n.head()
 
 
 # + id="JTcqWel-6Lq4"
 # 위에서 구한 결과를 판다스의 plot.bar()를 활용해 막대그래프로 그립니다.
+c.plot.bar(rot = 60)
 
 
 # + id="i2VY1UeP6Lq4"
 # "상권업종소분류명"이 "종합병원" 인 것과
 # "시도명" 이 "서울특별시"인 데이터만 가져옵니다.
 # df_seoul_hospital
+df_seoul_hospital = df[(df["상권업종소분류명"] == "종합병원") & (df["시도명"]=="서울특별시")].copy()
+df_seoul_hospital
 
 # + id="hFB2pHay6Lq5"
 # "시군구명" 으로 그룹화 해서 구별로 종합병원의 수를 세어봅니다.
+df_seoul_hospital["시군구명"].value_counts()
 
 
 # + [markdown] id="4ADiLacV6Lq6"
@@ -369,15 +454,38 @@ df.info()
 
 # + id="u0Jtq9En6Lq6"
 # str.contains 를 사용해서 "상호명"에 "대학병원"이 들어가는 것을 가져와서 head()로 미리보기 합니다.
+df_seoul_hospital.loc[~df_seoul_hospital["상호명"].str.contains("종합병원"), "상호명"].unique()
+# -
 
 
-# + id="_E-aNF-t6Lq7"
-# str.startswith 를 사용해 "서울"로 시작하는 도로명주소를 가져와서 head()로 미리보기 합니다.
+df_seoul_hospital[df_seoul_hospital["상호명"].str.contains("꽃배달")]
 
+df_seoul_hospital[df_seoul_hospital["상호명"].str.contains("의료기")]
 
-# + id="tx7ox72N6Lq8"
-# str.endswith를 사용해서 "상호명"이 "병원"으로 끝나는 데이터를 가져와서 head()로 미리보기 합니다.
+drop_row = df_seoul_hospital[
+    df_seoul_hospital["상호명"].str.contains("꽃배달|의료기|장례식장|상담소|어린이집")].index
+drop_row = drop_row.tolist()
+drop_row
 
+drop_row2 = df_seoul_hospital[
+    df_seoul_hospital["상호명"].str.endswith("의원")].index
+drop_row2 = drop_row2.tolist()
+drop_row2
+
+drop_row = drop_row + drop_row2
+len(drop_row)
+
+print(df_seoul_hospital.shape)
+df_seoul_hospital= df_seoul_hospital.drop(drop_row, axis=0)
+print(df_seoul_hospital.shape)
+
+df_seoul_hospital["시군구명"].value_counts().plot.bar()
+
+plt.figure(figsize=(15, 4))
+sns.countplot(data=df_seoul_hospital, x="시군구명", 
+              order=df_seoul_hospital["시군구명"].value_counts().index)
+
+df_seoul_hospital["상호명"].unique()
 
 # + [markdown] id="iSEfus-A6Lq8"
 # ### 특정 지역만 보기
@@ -387,31 +495,42 @@ df.info()
 # 결과를 df_seoul 이라는 데이터프레임에 저장합니다.
 # 새로운 변수에 데이터프레임을 저장시 copy()를 사용합니다.
 # df_seoul
+df_seoul = df[df["시도명"] == "서울특별시"].copy()
+df_seoul.shape
 
 
 # + id="rSKG78296Lq9"
 # seaborn 의 countplot 을 사용해서 위에서 만든 df_seoul 데이터프레임의
 # 시군구명을 시각화 합니다.
-plt.figure(figsize=(9, 6))
+# plt.figure(figsize=(9, 6))
+df_seoul["시군구명"].value_counts().plot.bar(figsize=(10,4), rot=30)
+# -
 
+
+plt.figure(figsize=(15, 4))
+sns.countplot(data=df_seoul, x="시군구명")
 
 # + id="cSIUGWlk6Lq-"
 # Pandas 의 plot.scatter 를 통해 경도와 위도를 표시해 봅니다.
+df_seoul[["경도", "위도", "시군구명"]].plot.scatter(x="경도", y="위도", figsize=(8, 7), grid=True)
 
 
 # + id="KY5QtbNJ6Lq_"
 # seaborn의 scatterplot 을 통해 구별 경도와 위도를 표시해 봅니다.
-plt.figure(figsize=(16, 12))
+plt.figure(figsize=(9,8))
+sns.scatterplot(data=df_seoul, x="경도", y="위도", hue="시군구명")
 
 
 # + id="snyOtXVt6LrA"
 # seaborn의 scatterplot 을 통해 "상권업종중분류명" 경도와 위도를 표시해 봅니다.
-plt.figure(figsize=(16, 12))
+plt.figure(figsize=(9,8))
+sns.scatterplot(data=df_seoul, x="경도", y="위도", hue="상권업종중분류명")
 
 
 # + id="nhjzDLZD6LrA"
 # seaborn의 scatterplot 을 통해 전국 데이터(df)로 구별 경도와 위도를 표시해 봅니다.
-plt.figure(figsize=(16, 12))
+plt.figure(figsize=(16,12))
+sns.scatterplot(data=df, x="경도", y="위도", hue="시도명")
 
 
 # + [markdown] id="HVffG2cU6LrB"
@@ -434,26 +553,29 @@ plt.figure(figsize=(16, 12))
 # https://anaconda.org/conda-forge/folium
 # conda install -c conda-forge folium
 # 지도 시각화를 위한 라이브러리
+import folium
+# -
 
 
-# + id="07q31tlE6LrC"
-# geo_df 에 df_seoul_hospital 을 copy() 로 복사해서 할당합니다.
+df_seoul_hospital["위도"].mean()
 
+df_seoul_hospital["경도"].mean()
 
-# + id="iAJv5sCL6LrD"
-# 지도의 중심을 지정하기 위해 위도와 경도의 평균을 구합니다.
+df_seoul_hospital.head(1)
 
+df_seoul_hospital.tail(1)
 
-# + [markdown] id="KxuCzBfi6LrD"
-# ## 좀 더 분석해 보기
-# * 상권업종중분류명, 상권업종소분류명 중에 관심 있는 분류명을 색인 후에 시도별, 시군구별 분석을 해보세요.
-# * 예시)
-#     * 언어치료는 시군구별로 어디에 많이 위치할까?
-#     * 의료시설은 서울과 강남에 집중되어 있을까?
-#     * 강남에는 피부과, 성형외과가 다른 지역에 비해 많을까?
+# +
+map = folium.Map(location=[df_seoul_hospital["위도"].mean(), df_seoul_hospital["경도"].mean()],
+                    zoom_start=12)
 
-# + id="cOTGwpDA6LrE"
-
-
-# + id="bV1Zp8CI6LrF"
-
+for n in df_seoul_hospital.index:
+    name = df_seoul_hospital.loc[n, "상호명"]
+    address = df_seoul_hospital.loc[n, "도로명"]
+    popup = f"{name}-{address}"
+    location = [df_seoul_hospital.loc[n, "위도"],df_seoul_hospital.loc[n, "경도"]]
+    folium.Marker(
+        location = location,
+        popup = popup,
+    ).add_to(map)
+map
